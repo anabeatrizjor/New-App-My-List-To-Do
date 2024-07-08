87b6e9fe46577a4f19d767cef77193a0462fa502
anabeatrizjor <biajordaoh@gmail.com>
2024-07-08 18:37:35 -0300

    App Splash Screen
    Inicialização da splash screen

--- app/src/main/java/com/example/mylistto_do/SplashActivity.kt
+++ app/src/main/java/com/example/mylistto_do/SplashActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mylistto_do
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,11 +12,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+     private var splashScreenMotion = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
         val logo = findViewById<ImageView>(R.id.logo)
+        val animation = AnimationUtils.loadAnimation(R.anim.animation_fade)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashScreenMotion.toLong())
     }
 }
