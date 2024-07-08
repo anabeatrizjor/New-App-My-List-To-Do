cbdaa311dae09ba549f22b567eeb9d19bed54fcc
anabeatrizjor <biajordaoh@gmail.com>
2024-07-08 18:05:46 -0300

    App My List To-Do
    App vazio

---
+++ app/src/main/java/com/example/mylistto_do/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mylistto_do
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        val logo = findViewById<ImageView>(R.id.logo)
+    }
+}
